a42b3152529b35ebfab4301d9d18cce98d5b2db9
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-02-03 13:04:59 +0100

    init commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/hello', function (Request $request) {
+    $users = DB::table('migrations')->get();
+    return $users;
+});
+Route::get('/env', function (Request $request) {
+    return env('DB_HOST');
+});
