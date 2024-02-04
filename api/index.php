c0bf61b2acd0b5153433b966b4f3ae7a28eaa1f8
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2024-02-04 12:36:58 +0100

    build: Initial Code change to make the deployment on vercel possible

--- api/index.php
+++ api/index.php
@@ -4,4 +4,14 @@
  * Here is the serverless function entry
  * for deployment with Vercel.
  */
-require __DIR__ . '/../public/index.php';
+$filename = $_SERVER['SCRIPT_NAME'] ?? '/index.php';
+
+if ($filename === '/index.php') {
+    require __DIR__ . '/../public/index.php';
+} else {
+    if (str_contains($filename, '.css'))
+        header('Content-Type: text/css');
+    if (str_contains($filename, '.js'))
+        header('Content-Type: application/javascript');
+    echo require __DIR__ . '/../public/' . $filename;
+}
